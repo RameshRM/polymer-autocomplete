27bb92e58a2e0e860805ecf88096b29462e3a31b
ramahadevan <ramahadevan@ebay.com>
2014-05-20 14:32:24 -0700

    added some media query

--- src/layouts/master-layout.rhtml.js
+++ src/layouts/master-layout.rhtml.js
@@ -4,7 +4,7 @@ module.exports = function create(__helpers) {
       raptor_templates_taglibs_layout_placeholder_tag = require("raptor-templates/taglibs/layout/placeholder-tag");
 
   return function render(data, context) {
-    context.w('<!doctype html> <html lang="en"><head><meta charset="UTF-8"><meta charset="utf-8"><title>AutoComplete</title><link rel="stylesheet" href="/css/app.css"><script src="/bower_components/platform/platform.js"></script><link rel="import" href="/auto-complete/auto-complete.html"></head><body><header><h1>Auto Complete</h1></header><div id="content"><main id="main">');
+    context.w('<!doctype html> <html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"><meta name="apple-mobile-web-app-capable" content="yes"><title>AutoComplete</title><link rel="stylesheet" href="/css/app.css"><script src="/bower_components/platform/platform.js"></script><link rel="import" href="/auto-complete/auto-complete.html"></head><body><header><h1>Auto Complete</h1></header><div id="content"><main id="main">');
     __helpers.t(context, 
       raptor_templates_taglibs_layout_placeholder_tag,
       {
