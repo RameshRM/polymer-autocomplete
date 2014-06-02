d6d9ca9fb765dfe23f95478e38ae90d694760ee2
rmrmail <rmrmail@gmail.com>
2014-06-02 16:16:11 -0700

    fixed to return keywords

--- src/controller/autocomplete.js
+++ src/controller/autocomplete.js
@@ -1,10 +1,4 @@
 module.exports = function(req, res) {
-    res.writeHead(200, {
-      'Content-Type': "application/json",
-      'Accept-Ranges': 'bytes',
-      'Cache-Control': 'no-cache'
-    });
-
     res.json({
         keywords: ['Red', 'Blue', 'Green', 'Yellow', 'Yelp', 'Yello Stone', 'Pink', 'Blah',
             'position', 'bottom', 'z-index', 'background-color', 'min-width', 'max-width', 'border-top-left-radius',
