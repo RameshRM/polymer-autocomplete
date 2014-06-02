6a13b6723c51d260bad2cbf27fd21e93c1591579
rmrmail <rmrmail@gmail.com>
2014-06-02 16:08:40 -0700

    added support for post

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
     app.get('/', require('./src/pages/index'));
     app.get('/keywords', require('./src/controller/autocomplete'));
+    app.post('/keywords', require('./src/controller/autocomplete'));
 };
