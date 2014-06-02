0c37a191ce8a5040e9ecdbf12aea793b92bd1c67
rmrmail <rmrmail@gmail.com>
2014-06-02 13:15:15 -0700

    added keywords route

--- router.js
+++ router.js
@@ -1,3 +1,4 @@
 module.exports = function(app) {
     app.get('/', require('./src/pages/index'));
+    app.get('/keywords', require('./src/controller/autocomplete'));
 };
---
+++ src/controller/autocomplete.js
@@ -0,0 +1,9 @@
+module.exports = function(req, res) {
+    res.json({
+        keywords: ['Red', 'Blue', 'Green', 'Yellow', 'Yelp', 'Yello Stone', 'Pink', 'Blah',
+            'position', 'bottom', 'z-index', 'background-color', 'min-width', 'max-width', 'border-top-left-radius',
+            'border-top-right-radius', 'border-bottom-right-radius', 'border-bottom-left-radius'
+        ]
+    });
+
+};
